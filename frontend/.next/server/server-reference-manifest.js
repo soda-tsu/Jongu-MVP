4b05a2fbdeae4fe11eed7ae640d86525d9f11602
soda-tsu <samuelcavalcantimg@gmail.com>
2025-07-28 15:14:38 -0300

    .next removida com sucesso

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KHUR+jw5i4P/VI2FDd02l84rs4ZUBwuja34HtZPaxOU=\"\n}"
--- frontend/.next/static/chunks/src_app_Books_page_jsx_2b7d18a3._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_Components_Working_Working_module_f04288e7.css"
-],
-    source: "dynamic"
-});
